ee8543f26b1b9179de55f6c2bf13e6e6531eb77e
Chirishman <Chirishman@users.noreply.github.com>
2018-01-19 19:00:46 -0500

    Added ability to pull full message
    Function pulls full message incl the body of emails.

---
+++ EWS/Get-EWSMessage.ps1
@@ -0,0 +1,20 @@
+﻿function Get-EWSMessage {
+    [OutputType('Microsoft.Exchange.WebServices.Data.EmailMessage')]
+    [CmdletBinding()]
+    Param(
+        [Parameter(
+                ParameterSetName = 'byId',
+                ValueFromPipelineByPropertyName,
+                Mandatory
+        )]
+        [Microsoft.Exchange.WebServices.Data.ItemId]$Id,
+        [Parameter(
+                ValueFromPipelineByPropertyName
+        )]
+        [Microsoft.Exchange.WebServices.Data.ExchangeService]$Service = $Script:exchangeService
+    )
+    $MailPropertySet = new-object Microsoft.Exchange.WebServices.Data.PropertySet([Microsoft.Exchange.WebServices.Data.BasePropertySet]::FirstClassProperties);
+    $MailPropertySet.RequestedBodyType = [Microsoft.Exchange.WebServices.Data.BodyType]::Text
+    $Email = [Microsoft.Exchange.WebServices.Data.EmailMessage]::Bind($Service,$Id,$MailPropertySet);
+    $Email
+}
